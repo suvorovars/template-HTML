fe5489121304a2241aa2e984246bfbc55c87fc42
suvorovars <suvorovars@gmail.com>
2021-11-06 14:01:08 +0300

    ....

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
